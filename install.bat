8abde1e48c905beeeb53fe4c484fffd78021704e
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-03-09 18:12:58 +0100

    improve windows scripts

--- install.bat
+++ install.bat
@@ -1,4 +1,10 @@
-RELEASE_URL="https://github.com/mamba-org/micromamba-releases/releases/latest/download/micromamba-win-64"
+REM Check if environment variable VERSION is set
+if "%VERSION%"=="" (
+    REM If not, set it to "latest"
+    SET VERSION=latest
+)
+
+RELEASE_URL="https://github.com/mamba-org/micromamba-releases/releases/%VERSION%/download/micromamba-win-64"
 
 REM Download micromamba using curl.exe
 curl.exe -L -o micromamba.exe %RELEASE_URL%
@@ -8,3 +14,34 @@ MKDIR %LOCALAPPDATA%\micromamba
 
 REM Move micromamba.exe to the final directory
 MOVE /Y micromamba.exe %LOCALAPPDATA%\micromamba\micromamba.exe
+
+REM check if this is an interactive shell
+if "%PROMPT%"=="" (
+    echo Initializing micromamba in %USERPROFILE%\micromamba
+    %LOCALAPPDATA%\micromamba\micromamba.exe init -p %USERPROFILE%\micromamba
+)
+
+@REM REM Ask user if micromamba should be added to the PATH
+@REM ECHO Add micromamba to the PATH?
+@REM ECHO y) Yes
+@REM ECHO n) No
+@REM SET /P ADD_TO_PATH="Enter your choice: "
+
+@REM REM check if add to path is either y or Y (case insensitive)
+@REM if "%ADD_TO_PATH:~0,1%"=="y" (
+@REM     REM Add micromamba to the PATH
+@REM     echo Adding micromamba to the PATH
+@REM     setx PATH "%PATH%;%LOCALAPPDATA%\micromamba"
+@REM )
+
+REM Ask user if micromamba should be initialized
+ECHO Initialize micromamba?
+ECHO y) Yes (default)
+ECHO n) No
+SET /P INITIALIZE="Enter your choice: "
+
+if "%INITIALIZE:~0,1%"=="y" || "%INITIALIZE"=="" (
+    REM Initialize micromamba
+    echo Initializing micromamba in %USERPROFILE%\micromamba
+    %LOCALAPPDATA%\micromamba\micromamba.exe init -p %USERPROFILE%\micromamba
+)
--- install.ps1
+++ install.ps1
@@ -1,7 +1,32 @@
-$RELEASE_URL="https://github.com/mamba-org/micromamba-releases/releases/latest/download/micromamba-win-64"
+# check if VERSION env variable is set, otherwise use "latest"
+$VERSION = if ($Env:VERSION -eq $null) { "latest" } else { $Env:VERSION }
 
+$RELEASE_URL="https://github.com/mamba-org/micromamba-releases/releases/$VERSION/download/micromamba-win-64"
+
+echo "Downloading micromamba from $RELEASE_URL"
 curl.exe -L -o micromamba.exe $RELEASE_URL
 
 New-Item -ItemType Directory -Force -Path  $Env:LocalAppData\micromamba
 
+echo "Installing micromamba to $Env:LocalAppData\micromamba"
 Move-Item -Force micromamba.exe $Env:LocalAppData\micromamba\micromamba.exe
+
+# check if this is an interactive session
+if ($Host.UI.RawUI -eq $null) {
+    echo "Not an interactive session, initializing micromamba to $Env:UserProfile\micromamba"
+    micromamba shell init -s powershell -p $Env:UserProfile\micromamba
+}
+
+# echo "Adding micromamba to PATH"
+# [Environment]::SetEnvironmentVariable("Path", "$Env:LocalAppData\micromamba;" + [Environment]::GetEnvironmentVariable("Path", "User"), "User")
+
+$choice = Read-Host "Do you want to initialize micromamba? (Y/n)"
+if ($choice -eq "y" -or $choice -eq "Y" -or $choice -eq "") {
+    $prefix = Read-Host "Enter the path to the micromamba prefix (default: $Env:UserProfile\micromamba)"
+    if ($prefix -eq "") {
+        $prefix = "$Env:UserProfile\micromamba"
+    }
+
+    echo "Initializing micromamba in  $prefix"
+    micromamba shell init -s powershell -p $Env:UserProfile\micromamba
+}
