b844419a3b804ef0b378c49cf8cfdf903cebcf7c
Hind Montassif <hind.montassif@gmail.com>
2025-01-09 16:31:58 +0100

    Mock copyfile

--- tests/test_fetch_release.py
+++ tests/test_fetch_release.py
@@ -97,9 +97,10 @@ def test_get_micromamba_non_existing_version(use_default_version):
 
 #TODO mock test for non existing versions => new_version_1_x, new_version_2_x, new_prerelease
 
-@patch('fetch_release.requests.get')
+@patch('fetch_release.requests.get') # TODO remove fetch_release?
 @patch("subprocess.check_call")
-def test_get_micromamba_new_2_x_version(mock_check_call, mock_get):
+@patch("shutil.copyfile")
+def test_get_micromamba_new_2_x_version(mock_get, mock_check_call, mock_copyfile):
     # Mock the response from the Anaconda API
     mock_response = MagicMock()
     mock_response.status_code = 200
@@ -181,6 +182,8 @@ def test_get_micromamba_new_2_x_version(mock_check_call, mock_get):
 
     # Mock subprocess.check_call to prevent actual command execution
     mock_check_call.return_value = None  # Simulate a successful call
+    # Mock shutil.copyfile to prevent file copying
+    mock_copyfile.return_value = None  # Simulate successful copy
 
     # Mock existing GitHub tags to simulate the version already being tagged
     with patch.object(fetch_release, 'get_all_tags_github', return_value={'2.0.5-0'}):
@@ -193,6 +196,11 @@ def test_get_micromamba_new_2_x_version(mock_check_call, mock_get):
         # Ensure that subprocess.check_call was called to extract the archive
         #mock_check_call.assert_called_once_with(
             #["micromamba", "package", "extract", "micromamba-10.11.12-1-linux-64.tar.bz2", "micromamba-10.11.12-1-linux-64"])
+
+        # Ensure shutil.copyfile is called with the expected paths
+        #mock_copyfile.assert_called_once_with(
+            #'micromamba-10.11.12-1-linux-64/bin/micromamba', 'releases/micromamba-linux-64'
+        #)
 
         assert get_output_value("MICROMAMBA_NEW_VERSION") == "true"
 
